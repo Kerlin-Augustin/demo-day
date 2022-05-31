06658cc473501bb5126f05bc52e49429a583beb2
Kerlin Augustin <kerlin.augustin1@gmail.com>
2022-05-30 23:47:24 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://kerlinaugustin:kerlin@moneypool.beeln.mongodb.net/moneypool?retryWrites=true&w=majority', 
+    'dbName': 'moneypool'
+};
